439325ec4a27cc6ddda0c22e590fa8552fc19705
rubayat-rafi <rubayatrafi522@gmail.com>
2025-01-15 01:55:13 +0600

    project setup react router dom install completed

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div>
+            This is Footer Section
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,10 @@
+
+const MainLayout = () => {
+    return (
+        <div>
+            hello main Layout
+        </div>
+    );
+};
+
+export default MainLayout;
